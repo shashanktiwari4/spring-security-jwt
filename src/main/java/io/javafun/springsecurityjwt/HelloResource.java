be29f2e66e6e7ffa26b4a20897ac1cf23dc713a1
shashanktiwari <shashank09tiwari@gmail.com>
2022-07-10 16:20:13 +0530

    Initial commit!

---
+++ src/main/java/io/javafun/springsecurityjwt/HelloResource.java
@@ -0,0 +1,16 @@
+package io.javafun.springsecurityjwt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+
+ @RequestMapping("/hello")
+  public String hello(){
+      return "Hello World";
+  }
+
+
+}
---
+++ src/main/java/io/javafun/springsecurityjwt/security/SecurityConfigurer.java
@@ -0,0 +1,54 @@
+package io.javafun.springsecurityjwt.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer  {
+
+    @Autowired
+    private MyUserdetailsService myUserdetailsService;
+    @Autowired
+    private  AuthenticationConfiguration configuration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Autowired
+    void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(myUserdetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // for development
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+
+
+}
